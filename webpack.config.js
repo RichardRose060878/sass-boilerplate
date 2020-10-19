fa59837e0dc5b8aeff8010b37e3d05af8d4810bc
Richard <richardrose060878@gmail.com>
2020-10-19 18:31:16 +0100

    sass boilerplate setup.

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require("fs-extra");
+
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("postcss-hexrgba"),
+  require("autoprefixer")
+];
+
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy images", function () {
+      fse.copySync("./src/assets/images", "./docs/assets/images");
+    });
+  }
+}
+
+let cssConfig = {
+  test: /\.scss$/i,
+  use: [
+    "css-loader?url=false",
+    {
+      loader: "postcss-loader",
+      options: { plugins: postCSSPlugins }
+    },
+    { loader: "sass-loader" }
+  ]
+};
+
+let pages = fse
+  .readdirSync("./src")
+  .filter(function (file) {
+    return file.endsWith(".html");
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./src/${page}`
+    });
+  });
+
+let config = {
+  entry: "./src/assets/scripts/App.js",
+  plugins: pages,
+  module: {
+    rules: [
+      cssConfig,
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader");
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "src")
+  };
+  config.devServer = {
+    before: function (app, server) {
+      server._watch("./src/**/*.html");
+    },
+    contentBase: path.join(__dirname, "src"),
+    hot: true,
+    port: 3000,
+    host: "192.168.1.40" // use ipconfing in the command prompt and add your machines ip address
+  };
+  config.mode = "development";
+}
+
+if (currentTask == "build") {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  postCSSPlugins.push(require("cssnano"));
+  config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "docs")
+  };
+  config.mode = "production";
+  config.optimization = {
+    splitChunks: { chunks: "all" }
+  };
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }),
+    new RunAfterCompile()
+  );
+}
+
+module.exports = config;
